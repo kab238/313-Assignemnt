4c7f5ac3ce79de05712c7c6de445c393dc2b631e
Kabir Bhargava <162187540+kab238@users.noreply.github.com>
2025-06-02 12:19:44 +1200

    task 2.3 partly done

---
+++ assignment2/task2.3.cpp
@@ -0,0 +1,193 @@
+//g++ task2.3.cpp smbPitchShift.cpp -o task2.3 -I/opt/homebrew/include -L/opt/homebrew/lib -lportaudio
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "portaudio.h"
+#include "smbPitchShift.h"
+#include <iostream>
+#include <vector>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include <queue>
+using namespace std;
+
+
+#define SAMPLE_RATE       (44100)
+#define FRAMES_PER_BUFFER   (1024)
+#define NUM_SECONDS          (15)
+#define BUFFER_SIZE        (1024)
+#define INPUT_CHANNEL_NO 1
+#define OUTPUT_CHANNEL_NO 1
+
+const long  OSAMP = 64;
+const PaSampleFormat SAMPLE_FORMAT = paFloat32;
+float pitchShift = 1;
+
+PaStreamParameters inputParameters, outputParameters;
+PaStream *stream = NULL;
+PaError err;
+
+mutex mutex_thread,mutex_pitch;
+float outputBuffer[BUFFER_SIZE];
+// initialises queue of audio buffers (each vector is a vector of float samples)
+std::queue<std::vector<float>> audioQueue; 
+std::condition_variable cond,pitch_enable;
+
+std::atomic<bool> finishedReading(false);
+std::atomic<bool> stop_requested(false);
+std::atomic<bool> pitch_enabled(false);
+
+static void checkErr(PaError err) {
+    if (err != paNoError) {
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void ReadAudioThread(PaStream *stream) {
+
+    while (!stop_requested) {
+        cout << "in read audio thread" << endl;
+    //*read the input
+    for( int i = 0; i<(NUM_SECONDS*SAMPLE_RATE)/BUFFER_SIZE; i++) {
+        
+        //create temporary input vector of float samples
+        std::vector<float> inputBuffer(BUFFER_SIZE);
+
+        //reads stream to input vector
+        err = Pa_ReadStream(stream,inputBuffer.data(),BUFFER_SIZE);
+        checkErr(err);
+        if (stop_requested) {return;}
+    {
+        // locks the mutex so PitchShiftThread doesn't have access & move input data to queue
+        std::lock_guard<std::mutex> lock(mutex_thread);
+        audioQueue.push(std::move(inputBuffer));
+    }
+    //notify pitchshift thread that one frame has been read to the shared vector
+        cond.notify_one();
+    }
+
+    // All samples have finsihed being read, toggle finish flag and notify other thread
+    finishedReading = true;
+    // {std::lock_guard<std::mutex> lock(mutex_thread);
+    // finishedReading = true;
+    // }
+    // cond.notify_one();
+    return;
+}
+cout << "still in read audio thread" << endl;
+}
+
+void PitchShiftThread(){
+
+    while (!stop_requested) {
+        //cout << "in pitch shift thread" << endl;
+        // gives access to the lock shared by both threads
+        std::unique_lock<mutex> lock(mutex_thread);
+
+        //waits to be notified for data to be read
+        cond.wait(lock, [](){return (!audioQueue.empty() || finishedReading);}); // spurious wake up condiiton
+
+        if (stop_requested) {return;}
+
+        // condiiton for when the duration of time is over
+        if (audioQueue.empty() && finishedReading) {
+            return;
+        }
+        std::vector<float> input = std::move(audioQueue.front());
+        audioQueue.pop();
+        lock.unlock();
+    if (pitch_enabled == true) {
+        smbPitchShift(pitchShift,BUFFER_SIZE,FRAMES_PER_BUFFER,OSAMP,SAMPLE_RATE,input.data(),outputBuffer);
+    }
+        // {   std::unique_lock<std::mutex> pitch_lock(mutex_pitch);
+        //     pitch_enable.wait(pitch_lock);
+            
+        // }
+        err = Pa_WriteStream(stream, outputBuffer, BUFFER_SIZE);
+        checkErr(err);
+    }
+    return;
+}
+
+void HotKeyThread() {
+    
+    char input;
+    int passthrough_flag = 0;
+    
+    while(1) {
+        if (finishedReading) {
+            cout << "times run out" << endl;
+            break;}
+        std:cin >> input;
+        //exits the program if 'q' is input
+        if (input == 'q') {
+            stop_requested = true;
+            finishedReading = true;
+            cond.notify_all();
+            break;
+        }
+        // if 's' is input the pitch shifter is enbaled in the pitchshift thread
+        if (input == 's') {
+            pitch_enabled = true;
+            // std::lock_guard<std::mutex> pitch_lock(mutex_pitch);
+            // pitch_enable.notify_one();
+        }
+        // if 'p' is input, pass through mode is enabled
+        if (input == 'p') {
+            pitchShift = 1;
+            passthrough_flag = 1;  
+        }
+        //if 'u' is input and the program isn't in passthrough mode add 0.5 units to the pitch shifter
+        if ((input == 'u') && (passthrough_flag == 1)) {
+           pitchShift += 0.5;
+        }
+        //if 'd' is input and the program isn't in passthrough mode decrease 0.5 units from the pitch shifter
+        if ((input == 'd') && (passthrough_flag == 1)) {
+            pitchShift -= 0.5;
+        }
+}}
+
+int main() {
+    
+    //initialising portAudio,checking input and output devices
+    err = Pa_Initialize();
+    checkErr(err);
+    int defaultInputDevice = Pa_GetDefaultInputDevice();
+    int defaultOutputDevice = Pa_GetDefaultOutputDevice();
+
+    if (defaultInputDevice == paNoDevice || defaultOutputDevice == paNoDevice) {
+        std::cerr << "No default input or output device found. " << std::endl;
+        Pa_Terminate();
+        return -1;
+    }
+
+    err = Pa_OpenDefaultStream(&stream, INPUT_CHANNEL_NO, OUTPUT_CHANNEL_NO, SAMPLE_FORMAT, SAMPLE_RATE,FRAMES_PER_BUFFER,nullptr,nullptr);
+    checkErr(err);
+    
+    err = Pa_StartStream(stream);
+    checkErr(err);
+
+    std::thread adjustemnt(HotKeyThread);
+    std::thread reader(ReadAudioThread,stream);
+    std::thread processor(PitchShiftThread);
+  
+    adjustemnt.join();
+    reader.join();
+    processor.join();
+    cout << "program finished" << endl;
+    //Stop input and output streams
+    err = Pa_StopStream(stream);
+    checkErr(err);
+
+    //Close input and output streams
+    err = Pa_CloseStream(stream);
+    checkErr(err);
+
+    //Terminate PortAudio
+    Pa_Terminate();
+
+    return 0;
+}
